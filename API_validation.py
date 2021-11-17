aecab4c36ce0a47f2d2797d2015ecca3fd33b105
OuChenghao <chenghao@57blocks.com>
2021-11-17 08:53:27 +0800

    2

--- API_validation.py
+++ API_validation.py
@@ -6,13 +6,15 @@ request = requests.session()
 for project_name in project_list:
     if project_name == "badger":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=badger&userAddr=0x60758B3A6933192D0Ac28Fc1f675364bb4dFAb1d&chain=Ethereum"
-        tag = ['deposit', 'yiel']
+        tag = ['deposit', 'yield']
         try:
             response = request.get(url)
             assert response.status_code == 200,"badger接口异常"
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"badger数据缺失"
-        except:
-            raise AssertionError
+        except AssertionError as e:
+            print(e)
+        finally:
+            continue
 
     elif project_name == "unsiwap":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=uniswap&userAddr=0x60758B3A6933192D0Ac28Fc1f675364bb4dFAb1d&chain=Ethereum"
@@ -22,6 +24,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == ['liquidity'],"uniswap数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "sushiswap":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=sushiswap&userAddr=0x60758B3A6933192D0Ac28Fc1f675364bb4dFAb1d&chain=Ethereum"
@@ -32,6 +36,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"sushiswap数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "aaveV2":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=aaveV2&userAddr=0x3cc542c4198990bcd1c98bc6af99865dceebc6a4&chain=Ethereum"
@@ -42,6 +48,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"aaveV2数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "aaveV1":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=aaveV1&userAddr=0x3cc542c4198990bcd1c98bc6af99865dceebc6a4&chain=Ethereum"
@@ -52,6 +60,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"aaveV1数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "compound":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=compound&userAddr=0x3cc542c4198990bcd1c98bc6af99865dceebc6a4&chain=Ethereum"
@@ -62,6 +72,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"compound数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "yearn":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=yearn&userAddr=0xA9f5422AC2454327f81503B65a3058b77Ad39545&chain=Ethereum"
@@ -71,6 +83,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == ['deposit'],"yearn数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "curve":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=curve&userAddr=0xA9f5422AC2454327f81503B65a3058b77Ad39545&chain=Ethereum"
@@ -80,6 +94,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == ['liquidity'],"curve数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
 
     elif project_name == "vesper":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=vesper&userAddr=0xA9f5422AC2454327f81503B65a3058b77Ad39545&chain=Ethereum"
@@ -90,7 +106,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"vesper数据缺失"
         except AssertionError as e:
             print(e)
-
+        finally:
+            continue
 
     elif project_name == "synthetix":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=synthetix&userAddr=0xc15025E98Ba033F9475785083e31B7811b0Fd0A4&chain=Ethereum"
@@ -101,7 +118,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"synthetix数据缺失"
         except AssertionError as e:
             print(e)
-
+        finally:
+            continue
 
     elif project_name == "origin":
         url = "https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=origin&userAddr=0xc15025E98Ba033F9475785083e31B7811b0Fd0A4&chain=Ethereum"
@@ -112,7 +130,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"origin数据缺失"
         except AssertionError as e:
             print(e)
-
+        finally:
+            continue
 
     elif project_name == "pancakeSwap":
         url = f"https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId={project_name}&userAddr=0xa37145a1edb49fcd376d49890c07454ea48cd313&chain=BSC"
@@ -123,7 +142,8 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"pancakeSwap数据缺失"
         except AssertionError as e:
             print(e)
-
+        finally:
+            continue
 
     elif project_name == "quickswap":
         url = f"https://tokenpad-api.dev.57blocks.com/defi/portfolio?projectId=quickswap&userAddr=0xa32321E3aAfc3C947c232f2CAbbDA043768e6953&chain=Polygon"
@@ -134,3 +154,7 @@ for project_name in project_list:
             assert sorted(jsonpath(response.json(),"$..tag")) == sorted(tag),"quickswap数据缺失"
         except AssertionError as e:
             print(e)
+        finally:
+            continue
+
+
